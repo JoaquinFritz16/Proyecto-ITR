1e312151e97dd4e7cccb82b3169b6b66b23b9e4b
Gorra.js <gorrahabla1234@gmail.com>
2024-09-26 10:57:27 -0300

    Version Inicial v1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+
+const options = {
+  method: 'GET',
+  url: 'https://online-movie-database.p.rapidapi.com/auto-complete',
+  params: {q: 'john wick'},
+  headers: {
+    'x-rapidapi-key': 'cb71281a97msh036212d3e3bc848p18e3e2jsn541ec39839d0',
+    'x-rapidapi-host': 'online-movie-database.p.rapidapi.com'
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0)

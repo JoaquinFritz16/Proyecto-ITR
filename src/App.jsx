b9cb65b900d17d38567799095497def0af6aba07
JoaquinFritz16 <162236965+JoaquinFritz16@users.noreply.github.com>
2024-09-27 11:28:23 -0300

    Fritz portada

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,12 @@ import "./App.css";
 import Portada from "./components/Portada";
 import axios from "axios";
 
-const options = {
-	method: "GET",
-	url: "https://online-movie-database.p.rapidapi.com/auto-complete",
-	params: { q: "john wick" },
-	headers: {
-		"x-rapidapi-key": "cb71281a97msh036212d3e3bc848p18e3e2jsn541ec39839d0",
-		"x-rapidapi-host": "online-movie-database.p.rapidapi.com",
-	},
-};
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
 
 function App() {
 	return (
 		<>
 			<Portada />
+      <h1>APi movies</h1>
 		</>
 	);
 }
